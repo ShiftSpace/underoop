c5031749b029a23b42a1d76b679c50789956cc2d
David Nolen <david.nolen@nytimes.com>
2010-05-20 12:25:15 -0400

    * options.js: follow HTML attribute naming rules

--- options.js
+++ options.js
@@ -1,6 +1,6 @@
 (function() {
 
-  function attributes (el) {
+  function attributes(el) {
     var attrs = {},
         attr;
     el = (_.isElement(el)) ? el : el[0];
@@ -10,16 +10,33 @@
     });
     return attrs;
   };
+
+  function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.substring(1).toLowerCase();
+  };
+
+  function dashesToCamelCase(str) {
+    return _(str.split("-")).map(function(s, i) {
+      if(i == 0) return s;
+      return capitalize(s);
+    }).join("");
+  };
   
   var root = this;
   root.Options = _.Module({
     name: "Options",
     getOptions: function(el, prefix) {
       prefix = "data-" + (prefix || "");
-      var r = new RegExp("^"+prefix);
-      return _(attributes(el)).filter(function(v, k) {
-        return k.search(r) != -1;
+      var r = new RegExp("^"+prefix),
+          options = {};
+      _(attributes(el)).each(function(v, k) {
+        if(k.match(r)) {
+          k = k.replace(prefix+"-", "");
+          k = dashesToCamelCase(k);
+          options[k] = v;
+        }
       });
+      return options;
     }
   });
   
