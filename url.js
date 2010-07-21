bec693e17eb3d1b6c01b35d1428abb0854490685
David Nolen <david.nolen@nytimes.com>
2010-07-21 14:22:34 -0400

    Added url.js to help with dealing with urls

---
+++ url.js
@@ -0,0 +1,67 @@
+jQuery(document.body).ready(init);
+
+var URL = _.Class({
+  initialize: function(options) {
+    this.protocol = options.protocol || "http";
+    this.host = options.host;
+    this.port = options.port || "";
+    this.path = options.path || "";
+    this.params = options.params || {};
+  },
+  queryString: function() {
+    return _(this.params).map(function(v, k) {
+      return k + "=" + v;
+    }).join("&");
+  },
+  addComponent: function(component) {
+    this.path = [this.path, component].join("/");
+  },
+  addParam: function(k, v) {
+    var temp = {};
+    temp[k] = v;
+    _(this.params).extend(temp);
+  },
+  toUrlString: function() {
+    if(!this.host) throw new Error("No host specified");
+    var portStr = (!this.port) ? "" : ":" + this.port,
+        qs = this.queryString(),
+        queryStr = qs ? "?" + qs : "";
+    return [this.protocol || "http", "://",
+            this.host,
+            portStr,
+            this.path,
+            queryStr].join("");
+  }
+});
+
+(function() {
+
+function parseParams(paramString) {
+  var xs = paramString.replace(/^\?/,'').split('&');
+  return _.reduce(xs, {}, function(memo, x) {
+    var temp = {},
+        kv = x.split("=");
+    temp[kv[0]] = kv[1];
+    return _(memo).extend(temp);
+  });
+};
+
+// adapted from http://snipplr.com/view/12659/parseurl/  
+URL.parse = function(urlString) {
+  var temp = jQuery("<a/>")[0];
+  temp.href = urlString;
+  console.log(temp);
+  return new URL({
+    protocol: temp.protocol.replace(":", ""),
+    host: temp.hostname,
+    port: temp.port,
+    path: temp.pathname.replace(/^([^\/])/,'/$1'),
+    params: parseParams(temp.search)
+  });
+};
+  
+})();
+
+function init() {
+  
+}
